7c22fa9c25df87194e43d84b714c7b491b3feba8
VasylPopaz <pafos1997@gmail.com>
2024-06-06 23:42:16 +0200

    add container, fonts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
 export const App = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-4xl text-slate-200">Vite + React + TypeScript</h1>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="font-inter-regular text-4xl ">
+        Vite + React + TypeScript
+      </h1>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,57 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "sm-max": { max: "374.99px" },
+      md: "768px",
+      lg: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    ({ addComponents }) => {
+      addComponents({
+        ".container": {
+          minWidth: "320px",
+          maxWidth: "375px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingLeft: "18px",
+          paddingRight: "18px",
+          "@screen md": {
+            paddingLeft: "32px",
+            paddingRight: "32px",
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            paddingLeft: "100px",
+            paddingRight: "100px",
+            maxWidth: "1440px",
+          },
+        },
+        ".scroll-up-btn": {
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: "49",
+          position: "fixed",
+          bottom: "-54px",
+          right: "5%",
+          padding: "10px",
+          border: "none",
+          width: "54px",
+          height: "54px",
+          backgroundColor: "#2355cc",
+          transition: "all 300ms ease-in-out",
+          "&:hover": {
+            backgroundColor: "#223f86",
+          },
+        },
+      });
+    },
+  ],
 };
