dce9b7c38e8bc24f6e4bdb9b6d72d95941d9e18f
VasylPopaz <pafos1997@gmail.com>
2024-06-07 18:35:22 +0200

    add useTheme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,14 @@ import { lazy } from "react";
 
 import { SharedLayout } from "../components";
 
+import { useTheme } from "../hooks";
+
 const Home = lazy(() => import("../pages/Home/Home"));
 const Psyhologists = lazy(() => import("../pages/Psyhologists/Psyhologists"));
 const Favorites = lazy(() => import("../pages/Favorites/Favorites"));
 
 export const App = () => {
+  const { theme, setTheme } = useTheme();
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export * from "./useEscapeKeyClose";
 export * from "./useModal";
+export * from "./useTheme";
